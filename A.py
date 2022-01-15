666b6407f48d3639371204869a1569c4886dd8ad
Fxomt <63350389+Fxomt-III@users.noreply.github.com>
2022-01-15 16:07:34 +0300

    Made the code even faster then before
    FAST

--- A.py
+++ A.py
@@ -1,97 +1,19 @@
-import sys
-
-def execute(filename):
-  f = open(filename, "r")
-  evaluate(f.read())
-  f.close()
-
-
-class _Getch:
-    """Gets a single character from standard input.  Does not echo to the
-screen."""
-    def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
-            self.impl = _GetchUnix()
-
-    def __call__(self): return self.impl()
-
-
-class _GetchUnix:
-    def __init__(self):
-        import tty, sys
-
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-
-class _GetchWindows:
-    def __init__(self):
-        import msvcrt
-
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
-
-
-getch = _Getch()
-
-def evaluate(code):
-  code     = cleanup(list(code))
-  bracemap = buildbracemap(code)
-
-  cells, codeptr, cellptr = [0], 0, 0
-
-  while codeptr < len(code):
-    command = code[codeptr]
-
-    if command == ">":
-      cellptr += 1
-      if cellptr == len(cells): cells.append(0)
-
-    if command == "<":
-      cellptr = 0 if cellptr <= 0 else cellptr - 1
-
-    if command == "+":
-      cells[cellptr] = cells[cellptr] + 1 if cells[cellptr] < 255 else 0
-
-    if command == "-":
-      cells[cellptr] = cells[cellptr] - 1 if cells[cellptr] > 0 else 255
-
-    if command == "[" and cells[cellptr] == 0: codeptr = bracemap[codeptr]
-    if command == "]" and cells[cellptr] != 0: codeptr = bracemap[codeptr]
-    if command == ".": sys.stdout.write(chr(cells[cellptr]))
-    if command == ",": cells[cellptr] = ord(getch())
-      
-    codeptr += 1
-
-
-def cleanup(code):
-  return ''.join(filter(lambda x: x in ['.', ',', '[', ']', '<', '>', '+', '-'], code))
-
-
-def buildbracemap(code):
-  temp_bracestack, bracemap = [], {}
-
-  for position, command in enumerate(code):
-    if command == "[": temp_bracestack.append(position)
-    if command == "]":
-      start = temp_bracestack.pop()
-      bracemap[start] = position
-      bracemap[position] = start
-  return bracemap
+# i strongly recommend you *the user* to run this in pypy, if you are using pypy, execute this command:
+#pypy -m pip install bfi
 
 import sys
 import os
+try:
+    import bfi
+except:
+    print("You do not have Brainf*ck interpreter installed, would you like to install it?")
+    x = input("(y/n): ")
+    if x == "y":
+        os.system("python -m pip install bfi")
+    else:
+        print("Goodbye!")
+    exit(1)
+
 # bfpp = open("bfpp.py", "r").read()
 
 # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
@@ -140,4 +62,4 @@ if len(sys.argv) > 1:
             f.write('\n'.join(code))
     else:
         code = AAA(open(sys.argv[1], "r").read(), "AAA")
-        evaluate(code)
+        bfi.interpret(code)
